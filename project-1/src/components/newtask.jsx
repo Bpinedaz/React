f02546122eae6489800da8d41db946fad5da570a
Bpinedaz <138085231+Bpinedaz@users.noreply.github.com>
2023-09-05 19:39:10 -0600

    Create newtask.jsx

---
+++ project-1/src/components/newtask.jsx
@@ -0,0 +1,17 @@
+export default function NewTask() {
+    return (
+
+        <div>
+
+<input type="text" placeholder="Add a new task"   />
+<button>Agregar</button>
+
+
+
+        </div>
+     
+     
+      
+      
+    );
+  }
